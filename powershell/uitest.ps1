666401d47136b6057ebaafb1527d8383381e7e61
js.kwak <theplow@naver.com>
2024-10-19 23:08:10 +0900

    test code

--- powershell/uitest.ps1
+++ powershell/uitest.ps1
@@ -1,7 +1,12 @@
+param (
+    [string]$Title,
+    [string]$SubTitle
+)
+
 # include functions
 . .\uiautomation.ps1
 
-$window = Get-ApplicationWindow -partialWindowTitle "*FreeCommander XE"
+$window = Get-ApplicationWindow -partialWindowTitle $Title
 if ($null -ne $window) {
-    CheckForPopup -rootElement $window -partialWindowTitle "FreeCommander"
+    CheckForPopup -rootElement $window -partialWindowTitle $SubTitle
 }
