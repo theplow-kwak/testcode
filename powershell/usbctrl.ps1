bad40f28aa033fd93b402770516c3df3f51f8958
js.kwak <theplow@naver.com>
2024-11-02 16:19:11 +0900

    add device enable/disable

---
+++ powershell/usbctrl.ps1
@@ -0,0 +1,18 @@
+# Set the search criteria for the device (e.g., by hardware ID)
+$targetDeviceID = "USB\VID_04E8&PID_6860&REV_0504"  # Replace with the actual hardware ID
+
+# Get device information using WMI
+$devices = Get-WmiObject Win32_PnPEntity
+
+# Change the active state of the found device (Disable)
+foreach ($device in $devices) {
+    # Write-Host "$($device.Name) : $($device.HardwareID)"
+    if ($device.HardwareID -like "*$targetDeviceID*") {
+        $device.PNPDeviceID
+        $device.Disable()
+        Write-Host "The device has been disabled."
+        Start-Sleep -Seconds 2
+        $device.Enable()
+        Write-Host "The device has been enabled."
+    }
+}
