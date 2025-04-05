91a4497a1de0430aa777e729f99fd58840abc547
js kwak <theplow@naver.com>
2025-04-05 10:11:01 +0900

    Refactor output file naming in combine.py and update audio file export functionality

--- python/moviepy/combine.py
+++ python/moviepy/combine.py
@@ -28,16 +28,17 @@ def combine_video(dir_path):
 dir_path = sys.argv[1] if len(sys.argv) > 1 else os.path.expanduser("~/hana/")
 clip = combine_video(dir_path)
 
-new_name = "cc_" + os.path.basename(os.path.normpath(dir_path)) + ".mp4"
+mp4_file_name = "cc_" + os.path.basename(os.path.normpath(dir_path)) + ".mp4"
+mp3_file_name = "cc_" + os.path.basename(os.path.normpath(dir_path)) + ".mp3"
 
 print(f"FPS: {clip.fps}, channels: {clip.audio.nchannels}")
 print(f"Width x Height of clip 1 : {clip.w} x {clip.h}")
 
 resize_clip = clip.resize(0.2)
 print(f"after resize Width x Height of clip 1 : {resize_clip.w} x {resize_clip.h}")
-clip.write_videofile(new_name)
+# clip.write_videofile(mp4_file_name, codec='libx264', fps=24, threads=4)
+clip.write_audiofile(mp3_file_name, 22050, codec='libmp3lame', ffmpeg_params=["-ac", "1"])
 
 # louder_clip = clip.audio.audio_normalize().volumex(10)
 # # print(f"channels: {clip.audio.nchannels}, louder_maxvol: {louder_clip.max_volume()}")
-
 # louder_clip.write_audiofile("louder_audio.mp3", 22050, codec='libmp3lame', ffmpeg_params=["-ac", "1"])
